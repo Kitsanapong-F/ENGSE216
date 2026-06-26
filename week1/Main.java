d824c5120129b3845970f214202f058bc3e2d865
Kitsanapong-F <09chaisu@gmail.com>
2026-06-26 10:22:00 +0700

    week1

---
+++ week1/Main.java
@@ -0,0 +1,14 @@
+package week1;
+public class Main {
+    public static void main(String[] args) {
+        collec c = new collec();
+        c.add(15);
+        c.add(7);
+        c.add(12);
+        c.add(16);
+        c.add(10);
+        c.remove(0);
+       System.out.println(c.size());
+       c.showall();
+    }
+}
