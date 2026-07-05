d2a75057666d355284db53997a8253b35698af46
Kitsanapong-F <09chaisu@gmail.com>
2026-07-05 23:49:07 +0700

    Listphonebook

--- week2/java/Main.java
+++ week2/java/Main.java
@@ -5,11 +5,10 @@ public class Main {
         List l = new List();
         l.add(5);
         l.add(6);
+        l.add(6);
         l.add(9);
         l.add(8);
         l.delete(0);
-        l.insert(0, 7);
         l.showall();
     }
-
 }
--- week2/phonebook/Listphonebook.java
+++ week2/phonebook/Listphonebook.java
@@ -1,6 +1,9 @@
 package week2.phonebook;
 
 public class Listphonebook {
-    
-    
+    String name[] = new String[5];
+    String lastName[] = new String[5];
+    String phoneNumber[] = new String[5];
+    int count;
+    boolean found = false;
 }
