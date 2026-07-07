c726c382b522d75cc552806dac3029956faad592
Kitsanapong-F <09chaisu@gmail.com>
2026-07-07 11:04:37 +0700

    Listphonebook

--- week2/phonebook/Listphonebook.java
+++ week2/phonebook/Listphonebook.java
@@ -4,6 +4,40 @@ public class Listphonebook {
     String name[] = new String[5];
     String lastName[] = new String[5];
     String phoneNumber[] = new String[5];
-    int count;
+    int count = 0;
     boolean found = false;
+
+    boolean isfull(){
+        return  count == this.name.length;
+    }
+
+    void add(String name,String lastName, String phoneNumber){
+        if(!isfull()){
+            this.name[count] = name;
+            this.lastName[count] =  lastName;
+            this.phoneNumber[count] = phoneNumber;
+            count++;
+        }
+        else{
+            System.out.println("Is Full");
+        }
+    }
+
+    void delete(){
+
+    }
+
+    boolean Insert(){
+        return found;
+    }
+
+    void sortData(){
+
+    }
+
+    void showAll(){
+        for(int index = 0 ; index < count; index++){
+            System.out.println("No : "+(1 + index)+" Name : "+this.name[index]+" LastName : "+this.lastName[index]+" PhoneNumber : "+this.phoneNumber[index]);
+        }
+    }
 }
--- week2/phonebook/Main.java
+++ week2/phonebook/Main.java
@@ -1,5 +1,15 @@
 package week2.phonebook;
 
 public class Main {
-    
+
+   public static void main(String[] args) {
+        Listphonebook l = new Listphonebook();
+        l.add("kitsanapondddg", "chaisu", "0987087578");
+        l.add("kitsanapong", "chaisu", "0987087578");
+        l.add("kitsanapong", "chaisu", "0987087578");
+        l.add("kitsanapong", "chaisu", "0987087578");
+        l.add("kitsanapong", "chaisu", "0987087578");
+        l.add("kitsanapong", "chaisu", "0987087578");
+        l.showAll();
+   }
 }
