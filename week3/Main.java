d5dae4e365261c0800bd8fb8cb2d2a4719c687bc
Kitsanapong-F <09chaisu@gmail.com>
2026-07-30 04:01:54 +0700

    test

---
+++ week3/Main.java
@@ -0,0 +1,16 @@
+package week3;
+
+public class Main {
+    public static void main(String[] args) {
+        CirclularQueue cq = new CirclularQueue(5);
+        cq.enqueue(1);
+        cq.enqueue(5);
+        cq.enqueue(3);
+        cq.enqueue(1);
+        cq.enqueue(1);
+        cq.enqueue(1);
+        cq.enqueue(1);
+        cq.showAll();
+        cq.showActive();
+    }
+}
